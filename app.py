b0715ad232f127d43ceb6882250ee344bad66119
josephoregon <josephoregon@icloud.com>
2020-12-22 19:15:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Making the necessary imports
-import streamlit as st
-import cv2
 import numpy as np
+import streamlit as st
+
 st.title("First Streamlit Web Application")
 st.markdown("This simple web application renders the uploaded images into grayscale mode.")
 
