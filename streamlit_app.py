535653bcd7198ed53dd41fab0fe09d228a00169b
JoVista <76548571+josephrosas@users.noreply.github.com>
2020-12-27 18:24:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,29 @@ def sharpen_img(image,
         low_contrast_mask = np.absolute(image - blurred) < threshold
         np.copyto(sharpened, image, where=low_contrast_mask)
     return sharpened
+  
+def adjust_gamma(image, gamma=1.0):
+  # build a lookup table mapping the pixel values [0, 255] to
+  # their adjusted gamma values
+  invGamma = 1.0 / gamma
+  table = np.array([((i / 255.0) ** invGamma) * 255
+      for i in np.arange(0, 256)]).astype("uint8")
+  # apply gamma correction using the lookup table
+  return cv2.LUT(image, table)
+
+def apply_gamma(image, amount=1.5):
+    # loop over various values of gamma
+    for gamma in np.arange(0.0, 3.5, 0.5):
+        # ignore when gamma is 1 (there will be no change to the image)
+        if gamma == 1:
+            continue
+        # apply gamma correction and show the images
+        gamma = gamma if gamma > 0 else 0.1
+        adjusted = adjust_gamma(image, gamma=gamma)
+        cv2.putText(adjusted, "g={}".format(gamma), (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+
+    return adjusted
 
 
 def get_circles(img, dp1, p1, p2, minr, maxr):
